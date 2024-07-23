147133627a0c328034bf3d59b3deaac9dd2e1574
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-07-23 11:44:34 +0900

    240723-s01

--- day_5/python_ws_5_5/ws_5_5.py
+++ day_5/python_ws_5_5/ws_5_5.py
@@ -1,14 +1,13 @@
 # 아래 함수를 수정하시오.
 def even_elements(input_list):
-    new_list = []
     for _ in range(len(input_list)):
         i = input_list.pop(0)
         if i % 2 == 0:
-            new_list.append(i)
-    
-    input_list.extend(new_list)
+            input_list.extend([i])
 
     return input_list
+
+# def even_elements(input_list):
 
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
